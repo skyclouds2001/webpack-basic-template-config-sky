6278adfe96143a512e4b4398228c0ee594232c0a
skyclouds2001 <skyclouds2001@163.com>
2022-09-20 10:42:38 +0800

    feat: change file max-size limit

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
                 use: {
                     loader: 'url-loader',
                     options: {
-                        limit: 1024 * 10,
+                        limit: 1024 * 8,
                         outputPath: 'img',
                     },
                 },
