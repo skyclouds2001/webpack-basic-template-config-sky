2e28bc7e220d88c2fa785beefade0919bd9da779
skyclouds2001 <skyclouds2001@163.com>
2022-10-20 22:25:00 +0800

    feat: small fix of stylelint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.{js,jsx,ts,tsx,vue}': ['npx eslint --fix'],
   '*.{js,jsx,ts,tsx,vue,md,html,css,sass,scss,less,styl,stylus}': ['npx prettier --write'],
-  '*.{css,sass,scss,less,styl,stylus,vue}': ['stylelint'],
+  '*.{css,sass,scss,less,styl,stylus,vue}': ['npx stylelint'],
 }
