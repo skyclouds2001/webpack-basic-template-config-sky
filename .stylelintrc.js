70a01c2d21be389012f1559f06ab48f8144d6546
skyclouds2001 <skyclouds2001@163.com>
2022-10-20 20:26:34 +0800

    feat: add css processor support for stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,27 @@
 module.exports = {
   extends: ['stylelint-config-recommended', 'stylelint-config-prettier'],
   plugins: ['stylelint-prettier'],
-  rules: {}
+  rules: {},
+  overrides: [
+    {
+      files: ['*.sass', '**/*.sass'],
+      customSyntax: 'postcss-sass',
+    },
+    {
+      files: ['*.scss', '**/*.scss'],
+      customSyntax: 'postcss-scss',
+    },
+    {
+      files: ['*.less', '**/*.less'],
+      customSyntax: 'postcss-less',
+    },
+    {
+      files: ['*.html', '**/*.html'],
+      customSyntax: 'postcss-html',
+    },
+    {
+      files: ['*.styl', '**/*.styl', '*.stylus', '**/*.stylus'],
+      customSyntax: 'postcss-styl',
+    },
+  ],
 }
